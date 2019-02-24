dc8e93bd0a360ffebd23d4561107d3fb3e2deb7b
James Adams <james@bluezen.co.uk>
2019-02-24 11:15:48 +0000

    Four space indentation

--- inputs_and_outputs/inputs_and_outputs.ino
+++ inputs_and_outputs/inputs_and_outputs.ino
@@ -13,39 +13,41 @@ bool point_1b = false;
 bool point_2a = false;
 bool point_2b = false;
 
-void setup() {
-  servo_point_1a.attach(6);
-  servo_point_1b.attach(9);
-  servo_point_2a.attach(10);
-  servo_point_2b.attach(11);
 
-  Serial.begin(9600, SERIAL_8N2); // make sure this matches your speed set in JMRI
-  for (int i=2; i<=5; i++) { pinMode(i, OUTPUT); }
-  for (int i=7; i<=8; i++) { pinMode(i, INPUT); digitalWrite(i, HIGH); }
+void setup() {
+    servo_point_1a.attach(6);
+    servo_point_1b.attach(9);
+    servo_point_2a.attach(10);
+    servo_point_2b.attach(11);
+
+    Serial.begin(9600, SERIAL_8N2); // make sure this matches your speed set in JMRI
+    for (int i=2; i<=5; i++) { pinMode(i, OUTPUT); }
+    for (int i=7; i<=8; i++) { pinMode(i, INPUT); digitalWrite(i, HIGH); }
 }
 
+
 void loop() {
-  // 1: build up a packet
-  cmri.process();
+    // 1: build up a packet
+    cmri.process();
 
-  // 2: update outputs
-  digitalWrite(2, cmri.get_bit(0));
-  digitalWrite(3, cmri.get_bit(1));
-  digitalWrite(4, cmri.get_bit(2));
-  digitalWrite(5, cmri.get_bit(3));
+    // 2: update outputs
+    digitalWrite(2, cmri.get_bit(0));
+    digitalWrite(3, cmri.get_bit(1));
+    digitalWrite(4, cmri.get_bit(2));
+    digitalWrite(5, cmri.get_bit(3));
 
-  // 2.1: update servo outputs
-  point_1a = cmri.get_bit(4);
-  point_1b = cmri.get_bit(5);
-  point_2a = cmri.get_bit(6);
-  point_2b = cmri.get_bit(7);
+    // 2.1: update servo outputs
+    point_1a = cmri.get_bit(4);
+    point_1b = cmri.get_bit(5);
+    point_2a = cmri.get_bit(6);
+    point_2b = cmri.get_bit(7);
 
-  servo_point_1a.write(30 * point_1a);
-  servo_point_1b.write(30 * point_1b);
-  servo_point_2a.write(30 * point_2a);
-  servo_point_2b.write(30 * point_2b);
+    servo_point_1a.write(30 * point_1a);
+    servo_point_1b.write(30 * point_1b);
+    servo_point_2a.write(30 * point_2a);
+    servo_point_2b.write(30 * point_2b);
 
-  // 3: update inputs (invert digitalRead due to the pullups)
-  cmri.set_bit(0, !digitalRead(7));
-  cmri.set_bit(1, !digitalRead(8));
+    // 3: update inputs (invert digitalRead due to the pullups)
+    cmri.set_bit(0, !digitalRead(7));
+    cmri.set_bit(1, !digitalRead(8));
 }
--- servo_tweaker/servo_tweaker.ino
+++ servo_tweaker/servo_tweaker.ino
@@ -8,81 +8,83 @@ bool gotPosition = false;
 #define SERVO_COUNT 6
 
 static int SERVO_PINS[SERVO_COUNT] = {
-  3,
-  5,
-  6,
-  9,
-  10,
-  11,
+    3,
+    5,
+    6,
+    9,
+    10,
+    11,
 };
+
 
 Servo servo;
 
+
 void moveServo(int id, int pos) {
-  // Connect servo, move, wait for move to complete and then disconnect to prevent jittering
-  if (id < SERVO_COUNT) {
-    servo.attach(SERVO_PINS[id]);
-    servo.write(pos);
-    delay(1500);
-    servo.detach();
-  }
+    // Connect servo, move, wait for move to complete and then disconnect to prevent jittering
+    if (id < SERVO_COUNT) {
+        servo.attach(SERVO_PINS[id]);
+        servo.write(pos);
+        delay(1500);
+        servo.detach();
+    }
 }
 
 
 void setup() {
-  Serial.begin(9600);
-  inputId.reserve(20);
-  inputPosition.reserve(20);
-  // Reset all servos to mid point
-  for (int i = 0; i < SERVO_COUNT; i++) {
-    moveServo(i, 90);
-  }
+    Serial.begin(9600);
+    inputId.reserve(20);
+    inputPosition.reserve(20);
+    // Reset all servos to mid point
+    for (int i = 0; i < SERVO_COUNT; i++) {
+        moveServo(i, 90);
+    }
 }
 
 
 void loop() {
-  if (gotId && gotPosition) {
-    int id = max(0, min(inputId.toInt(), 180));
-    int pos = max(0, min(inputPosition.toInt(), 180));
+    if (gotId && gotPosition) {
+        int id = max(0, min(inputId.toInt(), 180));
+        int pos = max(0, min(inputPosition.toInt(), 180));
 
-    Serial.print("| moving servo ");
-    Serial.print(id);
-    Serial.print(" to position ");
-    Serial.print(pos);
-    Serial.println(" |");
+        Serial.print("| moving servo ");
+        Serial.print(id);
+        Serial.print(" to position ");
+        Serial.print(pos);
+        Serial.println(" |");
 
-    moveServo(id, pos);
+        moveServo(id, pos);
 
-    // clear the inputs and reset states
-    inputId = "";
-    inputPosition = "";
-    gotId = false;
-    gotPosition = false;
-  }
+        // clear the inputs and reset states
+        inputId = "";
+        inputPosition = "";
+        gotId = false;
+        gotPosition = false;
+    }
 }
 
 
 void serialEvent() {
-  while (Serial.available()) {
-    char inChar = (char)Serial.read();
+    while (Serial.available()) {
+        char inChar = (char)Serial.read();
 
-    if (inChar == ' ') {
-      gotId = true;
-    } else if (inChar == '\n') {
-      gotId = true;
-      gotPosition = true;
-    } else if (!gotId) {
-      inputId += inChar;
-    } else if (!gotPosition) {
-      inputPosition += inChar;
-    }
+        if (inChar == ' ') {
+            gotId = true;
+        } else if (inChar == '\n') {
+            gotId = true;
+            gotPosition = true;
+        } else if (!gotId) {
+            inputId += inChar;
+        } else if (!gotPosition) {
+            inputPosition += inChar;
+        }
 
     /*
-    Serial.println("       inputId = " + inputId);
-    Serial.println(" inputPosition = " + inputPosition);
-    Serial.print("         gotId = "); Serial.println(gotId);
-    Serial.print("   gotPosition = "); Serial.println(gotPosition);
-    Serial.println("--------");
-    */
-  }
+        Serial.println("       inputId = " + inputId);
+        Serial.println(" inputPosition = " + inputPosition);
+        Serial.print("         gotId = "); Serial.println(gotId);
+        Serial.print("   gotPosition = "); Serial.println(gotPosition);
+        Serial.println("--------");
+        */
+    }
 }
