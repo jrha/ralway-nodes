edf27c3644b7858be0c67d2e5fc02e511625e131
James Adams <james@bluezen.co.uk>
2019-02-24 21:33:05 +0000

    next

--- bw_yard/bw_yard.h
+++ bw_yard/bw_yard.h
@@ -10,18 +10,18 @@
 
 #define SERVO_COUNT 5
 
-#define SERVO_STEP_DELAY 100
+#define SERVO_STEP_DELAY 50
 
 // Mapping of CMRI bit to arduino pin number for servos
 // Limits of servo travel for CLOSED ahead and THROWN positions
 // Normally PWM pins are used here
 static uint8_t SERVOS[SERVO_COUNT][4] = {
     // BIT, PIN, CLOSED, THROWN
-    {1,  5,  75, 115}, // First 3-way point, right-hand route
-    {2,  9,  90,  70}, // First 3-way point, left-hand route
-    {3,  6,  89,  91}, // Second 3-way point, right-hand route
-    {4, 10,  91,  81}, // Second 3-way point, left-hand route
-    {5, 11,  80, 110}, // Yard Stop Signal
+    {0,  5,  90,  70}, // First 3-way point, left-hand route
+    {1,  9,  75, 115}, // First 3-way point, right-hand route
+    {2,  6,  89,  91}, // Second 3-way point, right-hand route
+    {3, 10,  91,  81}, // Second 3-way point, left-hand route
+    {4, 11,  80, 110}, // Yard Stop Signal
 };
 
 
@@ -30,10 +30,10 @@ static uint8_t SERVOS[SERVO_COUNT][4] = {
 // Normally digital pins are used here
 static uint8_t OUTPUTS[OUTPUT_COUNT][2] = {
     // BIT, PIN
-    {6,  4},
-    {7,  7},
-    {8,  8},
-    {9, 12},
+    {5,  4},
+    {6,  7},
+    {7,  8},
+    {8, 12},
 };
 
 
@@ -42,21 +42,16 @@ static uint8_t OUTPUTS[OUTPUT_COUNT][2] = {
 // Normally digital and analog pins are used here
 static uint8_t INPUTS[INPUT_COUNT][2] = {
     // BIT, PIN
-    {A0,  1},
-    {A1,  2},
-    {A2,  3},
-    {A3,  4},
-    {A4,  5},
-    {A5,  6},
-    //{A6,  7}, //Analog only, needs manual conversion
-    //{A7,  8}, //Analog only, needs manual conversion
-    { 1,  9},
-    { 2, 10},
+    {0, A0},
+    {1, A1},
+    {2, A2},
+    {3, A3},
+    {4, A4},
+    {5, A5},
+    //{6, A6}, //Analog only, needs manual conversion
+    //{7, A7}, //Analog only, needs manual conversion
+    {8,  2},
+    {9,  3},
 };
-
-
-// State table for servo outputs
-// Used to prevent unnecessary servo activation
-bool servo_states[SERVO_COUNT];
 
 #endif
